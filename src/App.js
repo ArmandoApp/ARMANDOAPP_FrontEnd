69d2781ef36316b4598596fe9f34d6bf661a4223
Daniel porras <edward.porras@mail.escuelaing.edu.co>
2022-03-31 01:21:33 -0500

    lista

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/Ofertantes.css'
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Ofertantes from './components/ofertantes';
+import Conection from './components/conection';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+      
+          <Routes>
+
+            <Route path='/ofertantes' element={<Ofertantes/>}></Route>
+        </Routes>
+        <Routes>
+          <Route path='/lista' element={<Conection />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Components/conection.js
@@ -0,0 +1,24 @@
+
+import React from "react"
+
+
+
+const conection = function () {
+
+    let ofertantes = function () {
+
+
+
+
+    }
+
+    return (
+
+        <h1> HOlaa </h1>
+
+
+    );
+
+}
+
+export default conection;
