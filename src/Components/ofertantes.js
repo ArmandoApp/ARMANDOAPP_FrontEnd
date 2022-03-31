1fbee7192c541481018b8a6922be01182e4ef98d
Daniel porras <edward.porras@mail.escuelaing.edu.co>
2022-03-31 01:26:42 -0500

    infoUser

--- src/Components/ofertantes.js
+++ src/Components/ofertantes.js
@@ -1,25 +1,50 @@
-import React, { useEffect, useState } from "react";
-import ReactDOM from 'react-dom';
+import React, { useEffect, useState} from "react";
+
 import List from "@mui/material/List";
 import ListItem from '@mui/material/ListItem';
 import Avatar from "@mui/material/Avatar";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
 import ListItemText from "@mui/material/ListItemText";
 import Typography from "@mui/material/Typography";
-import ListItemButton from "@mui/material/ListItemButton"
+import ListItemButton from "@mui/material/ListItemButton";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Stack from "@mui/material/Stack";
+
+
+import Conection from './conection';
+
+
 import '../styles/Ofertantes.css'
+import { Navigate } from "react-router-dom";
 
 
 const Ofertantes = function() {
+    const style = {
+      position: "absolute",
+      top: "50%",
+      left: "50%",
+      transform: "translate(-50%, -50%)",
+      width: 400,
+      bgcolor: "background.paper",
+      border: "2px solid #000",
+      boxShadow: 24,
+      pt: 2,
+      px: 4,
+      pb: 3,
+    };
     const [data, setData] = useState(null);
-    const [reload, setReload] = useState('reload');
-    let g = "sdasd"
-    let lista = function () {
-
-    }
-
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => {
+      setOpen(true);
+    };
+    const handleClose = () => {
+      setOpen(false);
+    };
     useEffect(() => {
-        alert("entre")
+        
         fetch("http://localhost:8080/v1/Armando/Categorias/getCategories/Carpinteria")
             .then(response => response.json())
             .then(data => {
@@ -40,48 +65,93 @@ const Ofertantes = function() {
 
     }
 
+    let modal = function(){
+        console.log("entre")
+    
+       
+         return (
+           <Modal
+             hideBackdrop
+             open={open}
+             onClose={handleClose}
+             aria-labelledby="child-modal-title"
+             aria-describedby="child-modal-description"
+           >
+             <Box sx={{ ...style, width: 800, height: 900 }}>
+               <Conection />
+               <br></br>
+               <br></br>
+               <footer className="center">
+                 <Stack spacing={2} direction="row" className="center">
+                   <Button variant="contained" onClick={handleClose}>Volver</Button>
+                   <Button variant="outlined">Contactar</Button>
+                 </Stack>
+               </footer>
+             </Box>
+           </Modal>
+         );
+        
+        
+    }
+
 
     let listaJxc = function (props) {
 
-        return (<List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }} >
-            <ListItemButton onClick={() => { console.log(props.username) }}>
-                <ListItem alignItems="flex-start">
-                    <ListItemAvatar>
-                        <Avatar
-                            alt={props.username}
-                            src="/img/cruz.jpg"
-                            sx={{ width: 56, height: 56 }}
-                        />
-                    </ListItemAvatar>
-                    <ListItemText
-                        primary={props.username}
-                        secondary={
-                            <React.Fragment>
-                                <Typography
-                                    sx={{ display: "inline" }}
-                                    component="span"
-                                    variant="body2"
-                                    color="text.primary"
-                                >
-                                    {props.category}
-                                </Typography>
-                                {" — Listo para trabajar"}
-                            </React.Fragment>
-                        }
-                    />
-                </ListItem>
+        return (
+            
+          <List
+            sx={{
+              width: "100%",
+              maxWidth: 500,
+              bgcolor: "background.paper",
+              borderRadius: 10,
+              display: "block",
+            }}
+          >
+            <ListItemButton onClick={handleOpen} sx={{ borderRadius: 10 }}>
+              
+              <ListItem alignItems="flex-start">
+                <ListItemAvatar>
+                  <Avatar
+                    className="avatar"
+                    alt={props.username}
+                    src="/img/cruz.jpg"
+                    sx={{ width: 56, height: 56 }}
+                  />
+                </ListItemAvatar>
+                <div className="center">
+                  <ListItemText
+                    primary={props.username}
+                    secondary={
+                      <React.Fragment>
+                        <Typography
+                          sx={{ display: "inline" }}
+                          component="span"
+                          variant="body2"
+                          color="text.primary"
+                        >
+                          {props.category}
+                        </Typography>
+                        {" — Listo para trabajar"}
+                      </React.Fragment>
+                    }
+                  />
+                </div>
+              </ListItem>
             </ListItemButton>
-        </List>);
+          </List>
+        );
 
     }
 
 
     return (
-        <div className="container" >
-            <h1> CARPINTERIA </h1>
-            {ofertas()}
-        </div>
-  );
+      <div className="center">
+        <h1 className="center"> CARPINTERIA </h1>
+        <div className="center">{ofertas()}</div>
+        <div> {modal()} </div>
+      </div>
+    );
 }
 
 export default Ofertantes;
