497f6abc96e37987015d650e64bcac84b0a92aca
Daniel porras <edward.porras@mail.escuelaing.edu.co>
2022-03-31 01:32:31 -0500

    chat

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import './styles/Ofertantes.css'
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Ofertantes from './components/ofertantes';
 import Conection from './components/conection';
+import Chat from './components/Chat';
 
 function App() {
   return (
     <Router>
       <div className="App">
-      
-          <Routes>
-
-            <Route path='/ofertantes' element={<Ofertantes/>}></Route>
+        <Routes>
+          <Route path="/ofertantes" element={<Ofertantes />}></Route>
         </Routes>
         <Routes>
-          <Route path='/lista' element={<Conection />}></Route>
+          <Route path="/lista" element={<Conection />}></Route>
+        </Routes>
+        <Routes>
+          <Route path="/chat" element={<Chat />}></Route>
         </Routes>
       </div>
     </Router>
